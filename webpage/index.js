a0fa43cd4078ac4f66599a21f177ec156cdc7579
koal4z <55755687+koal4z@users.noreply.github.com>
2020-01-05 23:25:47 +0700

    add new website

---
+++ webpage/index.js
@@ -0,0 +1,8 @@
+lax.addPreset("logo", function() {
+	return { 
+		"data-lax-opacity": "(-vh*0.8) 40, (-vh*0.6) 0",
+		"data-lax-rotate": "(-vh*2) 1000, (-vh*0.5) 0" 
+	}
+})
+
+document.getElementById("section1").style.backgroundColor = "red"
